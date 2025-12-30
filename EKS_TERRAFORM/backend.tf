874eb6cc768b5004cd4ee8b5c247c5d45a17a1b5
kiranbabukk <kirankalyan.kt@gmail.com>
2025-12-30 12:37:40 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-kirankalyan123" # Replace with your actual S3 bucket name
+    bucket = "devsecops-harshkirankalyan-tetries" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
